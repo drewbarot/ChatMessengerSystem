fef97b47021023549e302d7ed6fd1c600f60574f
selmolla <selmolla@uwo.ca>
2020-03-28 10:12:22 -0400

    Placeholder fix

--- chat-messenger/src/layout/profileedit.js
+++ chat-messenger/src/layout/profileedit.js
@@ -9,8 +9,7 @@ const profileedit = ({
   children,
   ...props
 }) => {
-  // Manage the state whether to show the label or the input box. By default, label will be shown.
-// Exercise: It can be made dynamic by accepting initial state as props outside the component
+  // Manage the state whether to show the label or the input box.
   const [isEditing, setEditing] = useState(false);
 
 // Event handler while pressing any key while editing
@@ -21,7 +20,6 @@ const profileedit = ({
 /*
 - It will display a label is `isEditing` is false
 - It will display the children (input or textarea) if `isEditing` is true
-- when input `onBlur`, we will set the default non edit mode
 Note: For simplicity purpose, I removed all the classnames, you can check the repo for CSS styles
 */
   return (
